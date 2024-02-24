c044dad92377f63f5306b79c430d9aa3acdc5f81
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2024-02-24 09:35:00 +0300

    add autowired to linkvalidatormanager

--- bot/src/main/java/edu/java/bot/linkvalidators/LinkValidatorManager.java
+++ bot/src/main/java/edu/java/bot/linkvalidators/LinkValidatorManager.java
@@ -14,6 +14,7 @@ public final class LinkValidatorManager {
         return validators;
     }
 
+    @Autowired
     public LinkValidatorManager(List<LinkValidator> validators) {
         this.validators = validators;
     }
